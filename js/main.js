27e271221d372f3a24d07311fb21b9c6a4def51e
Arianna Delin <arianna.delin11@ufl.edu>
2015-09-25 17:11:30 -0400

    created a new functions branch and updated the js file

--- js/main.js
+++ js/main.js
@@ -1,2 +1,48 @@
 var iceCream = ["CookiesnCream", "Chocolate", "Mint"]; 
 	iceCream[iceCream.length] ="frenchVanilla"; 
+
+
+var Obama = {
+	firstName:"Barack"; 
+	lastName: "Obama"; 
+	termLength: "2"; 
+	party: "Democrat"; 
+	yearsOfPresidency: "5"; 
+}
+
+var BushSr = {
+	firstName:"George"; 
+	lastName: "Bush"; 
+	termLength: "2"; 
+	party: "Republican"; 
+	yearsOfPresidency: "8"; 
+}
+
+var Clinton = {
+	firstName:"Bill"; 
+	lastName: "Clinton"; 
+	termLength: "2"; 
+	party: "Democrat"; 
+	yearsOfPresidency: "8"; 
+}
+
+var BushJr = {
+	firstName:"George"; 
+	lastName: "Bush"; 
+	termLength: "1"; 
+	party: "Republican"; 
+	yearsOfPresidency: "4"; 
+}
+
+var Reagan ={
+	firstName:"Ronald"; 
+	lastName: "Reagan"; 
+	termLength: "2"; 
+	party: "Republican"; 
+	yearsOfPresidency: "8"; 
+}
+
+var presidents = ["Obama", "BushSr", "Clinton", "BushJr", "Reagan"]; 
+	console.log (2)
+
+
