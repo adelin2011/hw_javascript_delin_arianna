68c9a7ad1ab28cf9e16161423858e3f5cc28e137
Arianna Delin <arianna.delin11@ufl.edu>
2015-09-25 18:36:50 -0400

    added inception function to js file

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
+//arrays
 var iceCream = ["CookiesnCream", "Chocolate", "Mint"]; 
 	iceCream[iceCream.length] ="frenchVanilla"; 
 
-
+//presidents
 var Obama = {
 	firstName:"Barack"; 
 	lastName: "Obama"; 
@@ -45,4 +46,32 @@ var Reagan ={
 var presidents = ["Obama", "BushSr", "Clinton", "BushJr", "Reagan"]; 
 	console.log (2)
 
+//functions
+function howdy (person, message) {
+	return person + 'says' + message; 
+}
+var message = howdy ('Arianna Delin', 'Hi everyone!') 
+	console.log(message);
 
+
+var numba = 7; 
+
+if (string < numba){
+	console.log("What a short little word!")
+}
+else if (string > numba) {
+	console.log("I'm sorry, but that's too many to count.")
+}
+else if (string == numba){
+	console.log("7, what a perfect choice!")
+}
+
+function printtotheConsole(message){
+	console.log("7")
+}
+function printtotheConsole(message){
+	console.log("seventy")
+}
+function printtotheConsole(message){
+	console.log("university")
+}
